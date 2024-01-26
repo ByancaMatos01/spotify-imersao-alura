4b176fb9062e1be8fa0dc57f573b268667a09614
Byanca Matos <122841376+ByancaMatos01@users.noreply.github.com>
2024-01-26 16:55:06 -0300

    apiii funcionou dessa coisa amem aaaa
    muito frliz se fuder porra

--- spootify-imersão-alura/script.js
+++ spootify-imersão-alura/script.js
@@ -3,7 +3,7 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:6600/artists?name_like=${searchTerm}`
     fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
